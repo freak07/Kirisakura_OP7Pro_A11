fe1fb4d5727dcfa1468f8131b1c859134892a4a3
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-19 14:27:01 +0900

    treewide: remove remaining _no_log() usage
    sed -i -e 's/_no_log//g' $(git grep -l _no_log | tr '\n' ' ')
    
    and manually fix drivers/clk/qcom/clk-cpu-osm.c.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    (cherry picked from commit 75f9546505a343189a00abf3fc7ee20145c6b4dc)
    (cherry picked from commit 07be3a3157f2c3636133ca9ba7d28d689bd303c1)
    (cherry picked from commit 5a718ad45a41b98fe5cf0565e648b506844a7ac6)
    (cherry picked from commit 48aae934384da1cfe88320cdf496a635ee5b896b)
    (cherry picked from commit d856a0375b8ad83a1cb5d202285326d5475efde8)

--- drivers/thermal/tsens2xxx.c
+++ drivers/thermal/tsens2xxx.c
@@ -155,8 +155,7 @@ static int tsens2xxx_get_temp(struct tsens_sensor *sensor, int *temp)
 	sensor_addr = TSENS_TM_SN_STATUS(tmdev->tsens_tm_addr);
 	trdy = TSENS_TM_TRDY(tmdev->tsens_tm_addr);
 
-	code = readl_relaxed_no_log(trdy);
-
+	code = readl_relaxed(trdy);
 	if (!((code & TSENS_TM_TRDY_FIRST_ROUND_COMPLETE) >>
 		    TSENS_TM_TRDY_FIRST_ROUND_COMPLETE_SHIFT)) {
 		if (atomic_read(&in_tsens_reinit)) {
@@ -295,7 +294,7 @@ sensor_read:
 
 	tmdev->trdy_fail_ctr = 0;
 
-	code = readl_relaxed_no_log(sensor_addr +
+	code = readl_relaxed(sensor_addr +
 			(sensor->hw_id << TSENS_STATUS_ADDR_OFFSET));
 	last_temp = code & TSENS_TM_SN_LAST_TEMP_MASK;
 
@@ -304,7 +303,7 @@ sensor_read:
 		goto dbg;
 	}
 
-	code = readl_relaxed_no_log(sensor_addr +
+	code = readl_relaxed(sensor_addr +
 		(sensor->hw_id << TSENS_STATUS_ADDR_OFFSET));
 	last_temp2 = code & TSENS_TM_SN_LAST_TEMP_MASK;
 	if (code & TSENS_TM_SN_STATUS_VALID_BIT) {
@@ -313,7 +312,7 @@ sensor_read:
 		goto dbg;
 	}
 
-	code = readl_relaxed_no_log(sensor_addr +
+	code = readl_relaxed(sensor_addr +
 			(sensor->hw_id <<
 			TSENS_STATUS_ADDR_OFFSET));
 	last_temp3 = code & TSENS_TM_SN_LAST_TEMP_MASK;
