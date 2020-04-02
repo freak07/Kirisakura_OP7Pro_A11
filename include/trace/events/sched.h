e4f0bcd102a78b2a73e7b7d442414631ee258eb2
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-04-02 10:26:33 -0700

    sched: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: Iaefb893a84055748be7f2108179e3b869ac00318
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    (cherry picked from commit 227fa17a84ee33a930680d915e74b5780db03dfb)
    (cherry picked from commit 57397d1439d234442b329ae6e0c70c9931dc9720)
    (cherry picked from commit cde5346d8cdaf61ccf4038935bf0c59cb0a8efb5)

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -1498,6 +1498,7 @@ TRACE_EVENT(sched_task_util,
 		__field(int, start_cpu			)
 		__field(u32, unfilter			)
 		__field(unsigned long,  cpus_allowed	)
+		__field(bool,		low_latency)
 	),
 
 	TP_fast_assign(
@@ -1519,16 +1520,17 @@ TRACE_EVENT(sched_task_util,
 		__entry->rtg_skip_min		= rtg_skip_min;
 		__entry->start_cpu		= start_cpu;
 		__entry->unfilter		= p->unfilter;
+		__entry->cpus_allowed           = cpumask_bits(&p->cpus_allowed)[0];
+		__entry->low_latency		= p->low_latency;
 	),
 
-	TP_printk("pid=%d comm=%s util=%lu prev_cpu=%d next_cpu=%d backup_cpu=%d target_cpu=%d sync=%d need_idle=%d fastpath=%d placement_boost=%d latency=%llu uxtop=%d stune_boosted=%d is_rtg=%d rtg_skip_min=%d start_cpu=%d unfilter=%u affine=%#lx",
-		__entry->pid, __entry->comm, __entry->util, __entry->prev_cpu,
+	TP_printk("pid=%d comm=%s util=%lu prev_cpu=%d next_cpu=%d backup_cpu=%d target_cpu=%d sync=%d need_idle=%d fastpath=%d placement_boost=%d latency=%llu uxtop=%d stune_boosted=%d is_rtg=%d rtg_skip_min=%d start_cpu=%d unfilter=%u affine=%#lx low_latency=%d`",
 		__entry->next_cpu, __entry->backup_cpu, __entry->target_cpu,
 		__entry->sync, __entry->need_idle,
 		__entry->fastpath, __entry->placement_boost,
 		__entry->latency, __entry->stune_boosted, __entry->ux_task,
 		__entry->is_rtg, __entry->rtg_skip_min, __entry->start_cpu,
-		__entry->unfilter)
+		__entry->unfilter, __entry->cpus_allowed, __entry->low_latency)
 )
 
 /*
